c8f835757ed118ed5ca4715548733d981494063c
s3ksa <simon.sachet@protonmail.com>
2025-02-02 11:01:13 +0100

    0.1.2

--- Pion.py
+++ Pion.py
@@ -14,26 +14,43 @@ class Pion:
         self.pos_pion = ''
         self.list_pos_pion = []
         self.list_move = []
+        self.count_b = 0
+        self.count_n = 0
+        self.avant = 0
 
     def mouvement(self, pion: str, couleur):
         self.pos_pion = str(self.pos.case.get(pion)[0]) + str(self.pos.case.get(pion)[1])
         self.list_pos_pion = [int(self.pos_pion[0]), int(self.pos_pion[1])]
 
         if couleur == 'b':
-            avant = 1
+            self.avant = 1
 
         else:
-            avant = -1
+            self.avant = -1
 
-        if int(self.pos.position.get(self.pos_pion)[0]) + avant == '':
-            self.list_move.append([avant, 0])
+        if int(self.pos.position.get(self.pos_pion)[0]) + self.avant == '':
+            self.list_move.append([self.avant, 0])
 
-        self.eat.eat(str(self.list_pos_pion[0] + avant) + str(self.list_pos_pion[1] + 1))
-
-        if self.eat.possibilite:
-            self.list_move.append([avant, 1, self.eat.point])
-
-        self.eat.eat(str(self.list_pos_pion[0] + avant) + str(self.list_pos_pion[1] - 1))
+        self.eat.eat(str(self.list_pos_pion[0] + self.avant) + str(self.list_pos_pion[1] + 1))
 
         if self.eat.possibilite:
-            self.list_move.append([avant, -1, self.eat.point])
+            self.list_move.append([self.avant, 1, self.eat.point])
+
+        self.eat.eat(str(self.list_pos_pion[0] + self.avant) + str(self.list_pos_pion[1] - 1))
+
+        if self.eat.possibilite:
+            self.list_move.append([self.avant, -1, self.eat.point])
+
+    def promotion(self, pion):
+
+        if self.pos.case.get(pion)[0] + self.avant == 8 or self.pos.case.get(pion)[0] + self.avant == 1:
+
+            if self.avant == 1:
+                self.count_b += 1
+                self.pos.case[f'r{self.count_b}'] = self.pos.case.pop(pion)
+                self.pos.position[self.pos_pion] = f'r{self.count_b}'
+            
+            else:
+                self.count_n += 1
+                self.pos.case[f'r{self.count_n}'] = self.pos.case.pop(pion)
+                self.pos.position[self.pos_pion] = f'r{self.count_n}'
