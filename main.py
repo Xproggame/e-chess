ac70a6865ea73d24c59cf9798b2c9d0231647d89
s3ksa <simon.sachet@protonmail.com>
2025-02-08 10:29:59 +0100

    Vérification de la position

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Piece.fou import *
 from Verification.In import *
 from Verification.eat import *
 
-piece = Piece()
+info = Piece()
 pos = Position()
 conversion = Conversion(pos)
 out = In()
@@ -22,8 +22,15 @@ pion = Pion(pos, eat)
 
 arbre = []
 finish = False
+couleur_player = ''
+end = False
+case = ''
+coup = {
+    'p': pion.mouvement()
+}
 
 while True:
+    end = False
 
     while not finish:
         couleur_player = input("Quelle couleur voulez vous jouer ? [b, n] > ")
@@ -41,3 +48,30 @@ while True:
             finish = False
 
     finish = False
+
+    while not end:
+
+        if couleur_player == 'b':
+
+            while not finish:
+                case = input("Quelle piece voulez vous jouez ? [Coordonée] [chiffre, lettre] > ")
+                case_un = case[0] + conversion.convert.get(case[1])
+
+                for cle in conversion.convert.keys():
+
+                    if cle == case_un:
+
+                        if pos.position.get(case_un) != '':
+                            finish = True
+                            piece = pos.position.get(case_un)
+                            break
+
+                        else:
+                            print(f"[ERROR] Il n'y a pas de pièce à la position {case}.")
+                            break
+
+                    else:
+                        print(f"[ERROR] La position {case} n'est pas valide.")
+                        break
+
+            finish = False
