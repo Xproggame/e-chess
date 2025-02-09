d49874de635f2668c83793bf5e8192b756064be9
s3ksa <simon.sachet@protonmail.com>
2025-02-09 10:41:36 +0100

    Patch: Erreur d'importation.

--- Piece/test_Pion.py
+++ Piece/test_Pion.py
@@ -1,16 +1,16 @@
 from unittest import TestCase
-from Pion import Pion
-from Pos import Position
-from Eat import Eat
+from Piece.Pion import Pion
+from Info.Pos import Position
+from Verification.eat import Eat
 
 
 class TestPion(TestCase):
     def test_mouvement(self):
-        eat = Eat()
-        pos = Pos()
+        pos = Position
+        eat = Eat(pos)
         pion = Pion(pos, eat)
         pion.mouvement('pb8', 'b')
-        self.assertEquals(pion.list_move, ['83z'])
+        self.assertEquals(pion.list_move, ['83'])
 
     def test_promotion(self):
         self.fail()
